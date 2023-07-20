fdfbc3942402c8a559019ff66ec8e5d452171dc8
Caden <cadenkulp@gmail.com>
2023-07-20 07:47:14 -0700

    IM can view own items and all items

--- frontend/ui/src/pages/Inventory.js
+++ frontend/ui/src/pages/Inventory.js
@@ -7,11 +7,19 @@ import { Link } from 'react-router-dom';
 
 const Inventory = () => {
 
+  console.log('render')
+
   const [items, setItems] = useState([]);
+  const [viewingAllItems, setViewingAllItems] = useState(false);
 
   useEffect(() => {
+    let url = '';
     if ((cookie.parse(document.cookie)).loggedIn) {
-      let url = `http://localhost:3001/inventory?userId=${(cookie.parse(document.cookie)).userId}`;
+      if (viewingAllItems) {
+        url = `http://localhost:3001/inventory`;
+      } else {
+        url = `http://localhost:3001/inventory?userId=${(cookie.parse(document.cookie)).userId}`;
+      }
       fetch(url)
         .then(res => res.json())
         .then(data => {
@@ -19,7 +27,7 @@ const Inventory = () => {
           setItems(data)
         })
     } else {
-      let url = `http://localhost:3001/inventory`;
+      url = `http://localhost:3001/inventory`;
       fetch(url)
         .then(res => res.json())
         .then(data => {
@@ -27,23 +35,31 @@ const Inventory = () => {
           setItems(data)
         })
     }
-  }, []);
+  }, [viewingAllItems]);
 
   return (
     <>
       <Navigation />
       <Container className='mt-4'>
         {
-          (cookie.parse(document.cookie)).loggedIn ? 
-            <>
-              <h1>My Items</h1>
-              <Link to='/add-item' className='me-3'><Button>Add Item</Button></Link>
-            </> :
-            <h1>All Items</h1>
+          (cookie.parse(document.cookie)).loggedIn ?
+            viewingAllItems ? 
+              <>
+                <h1>All Items</h1>
+                <Link to='/add-item' className='me-3'><Button>Add Item</Button></Link>
+                <Button onClick={() => setViewingAllItems(false)}>View My Items</Button>
+              </>
+              :
+              <>
+                <h1>My Items</h1>
+                <Link to='/add-item' className='me-3'><Button>Add Item</Button></Link>
+                <Button onClick={() => setViewingAllItems(true)}>View All Items</Button>
+              </>
+            : <h1>All Items</h1>
         }
         <Row className='row-cols-4 mt-4'>
           {items.length > 0 ?
-            items.map(item => <Col><ItemCard item={item}/></Col>) :
+            items.map(item => <Col key={item.id}><ItemCard item={item}/></Col>) :
             <h1>You do not have any items</h1>}
         </Row>
       </Container>
